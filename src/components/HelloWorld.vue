746388fd8599596cbbfc82745ddd6040b5fe561d
Cavanaugh Richards <cavatron@pm.me>
2020-02-12 20:53:22 -0600

    update lint rules

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -104,9 +104,12 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
-    msg: String
+    msg: {
+      type: String,
+      default: ''
+    }
   }
 };
 </script>
